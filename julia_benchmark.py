7994cd72e3a4436e2f7187041c1f0ca75e97d80c
ahmad hassan <ahmadhassan@ahmads-MacBook-Pro.local>
2024-05-01 13:19:54 +0200

    added plotting to benchmarking

--- julia_benchmark.py
+++ julia_benchmark.py
@@ -3,6 +3,8 @@ import time
 from multiprocessing import Pool, TimeoutError
 import numpy as np
 from julia_par import compute_julia_in_parallel
+import matplotlib.pyplot as plt
+from julia_curve import c_from_group
 
 # section 2.2
 def run_benchmark(problem_sizes, patch_size, nprocs, num_experiments, xmin, xmax, ymin, ymax ,c):
@@ -25,7 +27,28 @@ def run_benchmark(problem_sizes, patch_size, nprocs, num_experiments, xmin, xmax
             results[size][n]["speedup"] = results[size][1]["mean_runtime"] / results[size][n]["mean_runtime"]
             results[size][n]["parallel_efficiency"] = results[size][n]["speedup"] / n
 
-    return results
+    print(results)
+
+    nprocs = list(results[155].keys())
+    mean_runtime_155, mean_runtime_1100 = [results[155][n]['mean_runtime'] for n in nprocs], [results[1100][n]['mean_runtime'] for n in nprocs]
+    speedup_155, speedup_1100  = [results[155][n]['speedup'] for n in nprocs], [results[1100][n]['speedup'] for n in nprocs]
+    parallel_eff_155, parallel_eff_1100 = [results[155][n]['parallel_efficiency'] for n in nprocs], [results[1100][n]['parallel_efficiency'] for n in nprocs]
+
+    def plot(x_values, y_values_155, y_values_1100, title, x_label, y_label):
+        plt.figure(figsize=(10, 6))
+        plt.bar([n - 0.22 for n in x_values], y_values_155, width=0.4, label='Size 155')
+        plt.bar([n + 0.22 for n in x_values], y_values_1100, width=0.4, label='Size 1100')
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        plt.title(title)
+        plt.xticks(nprocs)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    plot(nprocs, mean_runtime_155, mean_runtime_1100, 'Mean Runtime', 'Number of Processors', 'Mean Runtime (s)')
+    plot(nprocs, speedup_155, speedup_1100, 'Speedup', 'Number of Processors', 'Speedup')
+    plot(nprocs, parallel_eff_155, parallel_eff_1100, 'Parallel Efficiency', 'Number of Processors', 'Parallel Efficiency')
 
 if __name__ == '__main__':
 
@@ -34,7 +57,14 @@ if __name__ == '__main__':
     nprocs = [1, 2, 4, 8, 16, 24, 32]
     num_experiments = 3
     xmin, xmax, ymin, ymax = -1.5, 1.5, -1.5, 1.5
+    
     c = complex(-0.2, -0.65) 
+    run_benchmark(problem_sizes, patch_size, nprocs, num_experiments, xmin, xmax, ymin, ymax ,c)
+    
+    print("**********************************")
+    
+    c = c_from_group(2, 4) 
 
-    results = run_benchmark(problem_sizes, patch_size, nprocs, num_experiments, xmin, xmax, ymin, ymax ,c)
-    print(results)
+    run_benchmark(problem_sizes, patch_size, nprocs, num_experiments, xmin, xmax, ymin, ymax ,c)
+    
+    
